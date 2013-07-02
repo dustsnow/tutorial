f7b3aec5bc64e483fb68ba8caee0a09ea8f52265
penghou <dustsnow@gmail.com>
2013-07-01 22:01:27 -0400

    Finish CMake for ex1 and ex2. Error on ex3

---
+++ ex3-linking/build/CMakeFiles/libLocal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/libLocal.dir/subfolder/local.cpp.o"
+  "liblibLocal.pdb"
+  "liblibLocal.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/libLocal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ ex3-linking/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dustsnow/Documents/tutorial/ex3-linking/main.cpp" "/home/dustsnow/Documents/tutorial/ex3-linking/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/dustsnow/Documents/tutorial/ex3-linking/subfolder/local.cpp" "/home/dustsnow/Documents/tutorial/ex3-linking/build/CMakeFiles/main.dir/subfolder/local.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
