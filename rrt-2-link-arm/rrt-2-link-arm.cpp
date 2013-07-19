8b232753a40962624ad3aa0125e8d952347dbbcb
penghou <dustsnow@gmail.com>
2013-07-19 17:45:34 -0400

    Finish Documentation

--- rrt-2-link-arm/rrt-2-link-arm.cpp
+++ rrt-2-link-arm/rrt-2-link-arm.cpp
@@ -1,24 +1,17 @@
-/*
- *
- *
+/**
+ * @file rrt-2-link-arm.cpp
+ * @author Peng Hou
+ * @date July 19, 2013
+ * @brief This file implement the RRT algorithm with a 2 link arm
  */
-
-///input: 
-/// 	initial point(double pt_joint_init); 
-///     pt_goal point(double pt_joint_pt_goal)); 
-///     step size(double pt_joint_step);
-///output: a sequence of steps 
-///		output (matrix output)
 
 
 #include <flann/flann.hpp>
-//#include <flann/io/hdf5.h>
 #include "fcl/narrowphase/narrowphase.h"
 #include "fcl/collision.h"
 #include "fcl/collision_data.h"
 #include "fcl/collision_object.h"
 #include "fcl/math/transform.h"
-
 #include <fstream>
 #include <cmath>
 #include <math.h>
@@ -29,31 +22,25 @@ using namespace flann;
 using namespace std;
 using namespace fcl;
 
-int l1 = 1.0;//length of link 1
-int l2 = 2.0;//length of link 2
+int l1 = 1.0;///<length of link 1
+int l2 = 2.0;///<length of link 2
 
-int w = 0.1;
-double step_size = 0.01;
+int w = 0.1;///<width of links
+double step_size = 0.01;///<step size of small move
 
-bool violation_flag = false;// Collision Checker result. if true, pick a random point as pt_target; else set pt_target to pt_goal
-bool obstacle_flag = true;// If true, do collision check; else, do direct approach
+bool violation_flag = false;///< Collision Checker result. 
+bool obstacle_flag = true;///< If Obstacle presented 
 
-GJKSolver_libccd solver;
+GJKSolver_libccd solver;///< fcl collision checker solver
 
-void calcNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target,Matrix<double> pt_new);
-double calcDistance(Matrix<double> pt1, Matrix<double> pt2);
-void workspaceConversion(Matrix<double> *workspace, Matrix<double> jointspace);
-void pi(Index<L2<double> > index,int a);
-void calcCenterPoint(Matrix<double> pt,Matrix<double> *cp);
-bool collisionCheck(Matrix<double> pt);
-void findLinkset(vector<Matrix<double> > linkset);
-void printLinkset(vector<Matrix<double> > linkset);
+void CalcNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target,Matrix<double> pt_new);
+double CalcDistance(Matrix<double> point1, Matrix<double> point2);
+void CalcCenterPoint(Matrix<double> point,Matrix<double> *cp);
+bool CollisionCheck(Matrix<double> point);
+void FindPath(vector<Matrix<double> > linkset);
+double CalcVectorLength(Matrix<double> vector);
 
 int main(int argc, char** argv){
-	// Obstacles
-	//Box obs1(0.4,0.4,0);
-	//Transform3f tf_obs1(Vec3f(2.22,1,0));
-
 	// Center point for two links
 	Matrix<double> pt_cp(new double[4],2,2);
 	pt_cp[0][0] = 0;
@@ -61,15 +48,14 @@ int main(int argc, char** argv){
 	pt_cp[1][0] = 0;
 	pt_cp[1][1] = 0;
 
-    /// Test points
+    // Test points
 	Matrix<double> pt_test(new double[2],1,2);
 	pt_test[0][0] = 0.7854;
 	pt_test[0][1] = 0;
 
-	/// RRT data of nodes. 
+	// RRT data of nodes. 
 	Matrix<double> dataset(new double[2],1,2); 
-
-	/// Set root point as initial point
+	// Set root point as initial point
 	dataset[0][0] = 0.1;
 	dataset[0][1] = 0.2;
 
@@ -77,19 +63,19 @@ int main(int argc, char** argv){
 	vector<Matrix<double> > linkset;
 
 
-	///Construct index
+	//Construct index
 	Index<L2<double> > index(dataset,flann::KDTreeIndexParams(4));
 	index.buildIndex();
 
-	/// Target point matrix(randomly picked point or pt_goal point)
+	// Target point matrix(randomly picked point or pt_goal point)
 	Matrix<double> pt_target(new double[2],1,2);
 
-	/// zero point 
+	// zero point 
 	Matrix<double> pt_zero(new double[2],1,2);
 	pt_zero[0][0] = 0;
 	pt_zero[0][1] = 0;
 
-	/// pt_goal point 
+	// pt_goal point 
 	Matrix<double> pt_goal(new double[2],1,2);
 	pt_goal[0][0] = 0.45;
 	pt_goal[0][1] = 1.83;
@@ -102,16 +88,16 @@ int main(int argc, char** argv){
 	int number_of_points = 0;
 	while(true){
 
-		/// RRT data of links. First row is self node, second row is parent node
+		// RRT data of links. First row is self node, second row is parent node
 		//the first node is root node. Since root doesn't have parent node, make the second row as the root itself.
 		Matrix<double> link(new double[4],2,2); 
 
-	    /// pt_nearest point 
+	    // pt_nearest point 
 		Matrix<double> pt_nearest(new double[2],1,2);
 		pt_nearest[0][0] = 0.1;
 		pt_nearest[0][1] = 0.2;
 
-		/// pt_new point 
+		// pt_new point 
 	    Matrix<double> pt_new(new double[2],1,2);
 
 		if (violation_flag) {
@@ -124,29 +110,29 @@ int main(int argc, char** argv){
 		    pt_target[0][1] = pt_goal[0][1];
 		}
 
-		/// Find Nearest Neighbor Point(pt_nearest)
+		// Find Nearest Neighbor Point(pt_nearest)
 		index.knnSearch(pt_target, indices, dists, nn, flann::SearchParams(128));
 		pt_nearest[0][0] = (index.getPoint(indices[0][0]))[0];
         pt_nearest[0][1] = (index.getPoint(indices[0][0]))[1];
 
 
-		calcNewPoint(pt_nearest, pt_target, pt_new);
+		CalcNewPoint(pt_nearest, pt_target, pt_new);
 
 		// If obstacles present, do collision check on new point
 		if(obstacle_flag){
-			violation_flag = collisionCheck(pt_new);	
+			violation_flag = CollisionCheck(pt_new);	
 			if(violation_flag){
 				continue;
 			}
 		} 
-		/// Calculate distance between new point and pt_goal point(d). 
-		/// If d less than or equal to the threshold of the pt_goal point, this is the good enough result. Done
-		if(calcDistance(pt_goal, pt_new) <= step_size) break;
+		// Calculate distance between new point and pt_goal point(d). 
+		// If d less than or equal to the threshold of the pt_goal point, this is the good enough result. Done
+		if(CalcDistance(pt_goal, pt_new) <= step_size) break;
 
-		/// If collision check pass, Add new point and new edge to tree
+		// If collision check pass, Add new point and new edge to tree
 		index.addPoints(pt_new);
 	
-		/// put pt_new and pt_nearest into path
+		// put pt_new and pt_nearest into path
 		link[0][0] = pt_new[0][0];
         link[0][1] = pt_new[0][1];
         link[1][0] = pt_nearest[0][0];
@@ -159,59 +145,55 @@ int main(int argc, char** argv){
 			<< violation_flag << " "
 			<< endl;
 	}
-	findLinkset(linkset);
+	FindPath(linkset);
 	return 1;
 }
-/// Print the index
-void pi(Index<L2<double> > index,int a){
-	Matrix<double> pt_test(new double[2],1,2);
-	for(int i = 0; i < a;i++){
-		pt_test[0][0] = (index.getPoint(i))[0];
-		pt_test[0][1] = (index.getPoint(i))[1];
-		cout << pt_test[0][0]<<"\t"<<pt_test[0][1]<<endl;
-	}
-}
-/// print a point
-void pp(Matrix<double> pt){
-	cout << pt[0][0] << "\t"
-	     << pt[0][1] << endl;
-}
-/// assign a value of a point
-void ap(Matrix<double> *pt,double a, double b){
-	(*pt)[0][0] = a;
-	(*pt)[0][1] = b;
+
+/**
+ * @brief Calculate the distance of two points
+ * @param[in] point1,point2 Points consist of the coordinates
+ * @return The distance of two points
+ */
+double CalcDistance(Matrix<double> point1, Matrix<double> point2){
+	double x1 = point1[0][0];
+    double y1 = point1[0][1];
+    double x2 = point2[0][0];
+    double y2 = point2[0][1];
+	return sqrt(pow((x1 - x2),2)+pow((y1 - y2),2));
 }
 
-/// Calculate the distance of two points
-double calcDistance(Matrix<double> pt1, Matrix<double> pt2){
-	double x1 = pt1[0][0];
-    double y1 = pt1[0][1];
-    double x2 = pt2[0][0];
-    double y2 = pt2[0][1];
-	double distance = sqrt(pow((x1 - x2),2)+pow((y1 - y2),2));
-	return distance;
-}
-
-/// Calculate the pt_new from pt_nearest and pt_target
-void calcNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target, Matrix<double> pt_new){
+/**
+ * @brief Simulate "move a small step"
+ *
+ * Calculate the new point from nearest neighbor point and target point
+ *
+ * @param[in] pt_nearest nearest neighbor point found in index			 
+ * @param[in] pt_target target point picked randomly or set as goal point
+ * @param[in,out] pt_new the new point calculated
+ */
+void CalcNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target, Matrix<double> pt_new){
 	double x_nearest = pt_nearest[0][0];
 	double y_nearest = pt_nearest[0][1];
 	double x_target = pt_target[0][0];
 	double y_target = pt_target[0][1];
-	double distance = calcDistance(pt_target,pt_nearest);
+	double distance = CalcDistance(pt_target,pt_nearest);
 	pt_new[0][0] = (x_target - x_nearest)*step_size/distance+x_nearest;
 	pt_new[0][1] = (y_target - y_nearest)*step_size/distance+y_nearest;
 }
-/// Check the collision of the arm with obstacle presented
-/// Parameter
-///		pt: a double matrix containing the joint values
-bool collisionCheck(Matrix<double> pt){
-	/*
-	 * construct rect for links
-	 *		set height and width
-	 * 		set center point to "finally espected" position
-	 *		rotate the joint value 
-	 */
+
+/**
+ * @brief Calculate the length of vector
+ * @param[in] vector a space vector consist of three coordinates
+ */
+double CalcVectorLength(Matrix<double> vector){
+	return sqrt( pow(vector[0][0],2)+ pow(vector[0][1],2)+pow(vector[0][2],2) );	
+}
+
+/** 
+ * @brief Check the collision of the arm with obstacle presented
+ * @param[in] point the point consists of three joint values of the arm
+ */
+bool CollisionCheck(Matrix<double> point){
 	// Obstacles
 	Box obs1(0.4,0.4,0);
 	Transform3f tf_obs1(Vec3f(2.22,1.5,0));
@@ -220,7 +202,7 @@ bool collisionCheck(Matrix<double> pt){
 
 	// Calculate center point(cp)
 	Matrix<double> cp(new double[4],2,2);
-	calcCenterPoint(pt,&cp);
+	CalcCenterPoint(point,&cp);
 
 	// Create two rectangulers represent 2 link	
 	Box link1(l1,w,0);
@@ -229,37 +211,38 @@ bool collisionCheck(Matrix<double> pt){
 	Transform3f tf_link2(Vec3f(cp[1][0],cp[1][1],0));
 
 	// Rotate links at their joint angle
-	double theta1 = pt[0][0];
+	double theta1 = point[0][0];
 	fcl::Matrix3f rotation_matrix_1(cos(theta1),-sin(theta1),0,sin(theta1),cos(theta1),0,0,0,1);
 	tf_link1.setRotation(rotation_matrix_1);	
-	double theta2 = pt[0][0]+pt[0][1];
+	double theta2 = point[0][0]+point[0][1];
 	fcl::Matrix3f rotation_matrix_2(cos(theta2),-sin(theta2),0,sin(theta2),cos(theta2),0,0,0,1);
 	tf_link2.setRotation(rotation_matrix_2);	
 
-	/// Now the arm is properly positioned
-	/// Do collision check
+	// Now the arm is properly positioned
+	// Do collision check
 	double collision = solver.shapeIntersect(link1,tf_link1,obs1,tf_obs1,NULL,NULL,NULL);
 	collision = collision || solver.shapeIntersect(link2,tf_link2,obs1,tf_obs1,NULL,NULL,NULL);
 	return collision;
 }
-void calcCenterPoint(Matrix<double> pt,Matrix<double> *cp){
-	(*cp)[0][0] = l1/2.0 * cos(pt[0][0]);
-	(*cp)[0][1] = l1/2.0 * sin(pt[0][0]);
-	(*cp)[1][0] = 2.0*(*cp)[0][0] + l2/2.0 * cos(pt[0][0]+pt[0][1]);
-	(*cp)[1][1] = 2.0*(*cp)[0][1] + l2/2.0 * sin(pt[0][0]+pt[0][1]);
+
+/**
+ * @brief Calculate the center point of every link of the arm
+ * @param[in] point Consists of three joint values of the arm
+ * @param[in,out] center_points Stores center points of every link of the arm
+ */
+void CalcCenterPoint(Matrix<double> point,Matrix<double> *center_points){
+	(*center_points)[0][0] = l1/2.0 * cos(point[0][0]);
+	(*center_points)[0][1] = l1/2.0 * sin(point[0][0]);
+	(*center_points)[1][0] = 2.0*(*center_points)[0][0] + l2/2.0 * cos(point[0][0]+point[0][1]);
+	(*center_points)[1][1] = 2.0*(*center_points)[0][1] + l2/2.0 * sin(point[0][0]+point[0][1]);
 
 }
-void printLinkset(vector<Matrix<double> > linkset){
-	ofstream out("cpp_plot/linkset",ofstream::app);
 
-	for(int i = 0; i < linkset.size(); i++){
-		out << linkset[i][0][0] << " "
-			<< linkset[i][0][1] << " "
-			<< linkset[i][1][0] << " "
-            << linkset[i][1][1] << " " << endl;
-	}
-}
-void findLinkset(vector<Matrix<double> > linkset){
+/** 
+ * @brief Finding a path from RRT
+ * @param[in] linkset Containing nodes and links of RRT
+ */
+void FindPath(vector<Matrix<double> > linkset){
 	double size = linkset.size();
 	double *pt_child = linkset.back()[0];
 	double *pt_parent = linkset.back()[1];
@@ -285,59 +268,4 @@ void findLinkset(vector<Matrix<double> > linkset){
 		}
 	}
 }
-/* 
- * Convert from joint space to workspace
- * Parameters:
- *		Matrix<double> workspace
- *			workspace[0][0]:	x coordinate of node1
- *			workspace[0][1]:	y coordinate of node1
- *			workspace[1][0]:	x coordinate of node2
- *			workspace[1][1]:	y coordinate of node2
- *			workspace[2][0]:	x coordinate of node3
- *			workspace[2][1]:	y coordinate of node3
- * 		Matrix<double> jointspace
- *			jointspace[0][0]: joint value 1
- *			jointspace[0][1]: joint value 2
- */
-//void jointsp2worksp(Matrix<double> *jointspace, Matrix<double> workspace){
-//	double theta1 = jointspace[0][0];
-//	double theta2 = jointspace[0][1];
-//	(workspace)[1][0] = l1 * cos(theta1);
-//	(workspace)[1][1] = l1 * sin(theta1);
-//	(workspace)[2][0] = l2 * cos(theta1 + theta2) + (*workspace)[1][0];
-//	(workspace)[2][1] = l2 * sin(theta1 + theta2) + (*workspace)[1][1];
-//}
-//
-//void worksp2jointsp(Matrix<double> *workspace, Matrix<double> jointspace){
-//	double theta1 = jointspace[0][0];
-//	double theta2 = jointspace[0][1];
-//	(*workspace)[1][0] = l1 * cos(theta1);
-//	(*workspace)[1][1] = l1 * sin(theta1);
-//	(*workspace)[2][0] = l2 * cos(theta1 + theta2) + (*workspace)[1][0];
-//	(*workspace)[2][1] = l2 * sin(theta1 + theta2) + (*workspace)[1][1];
-//}
 
-
-void printWorkspace(Matrix<double> pt){
-}
-//Matrix<double> dataset;
-//load_from_file(dataset,"dataset.h5","dataset");
-
-//Matrix<double> pt_joint_goal (new double[2],1,2);
-//(pt_joint_goal.ptr())[0] = 5;
-//(pt_joint_goal.ptr())[1] = 6;
-//index.addPoints(pt_joint_init);
-//index.addPoints(pt_joint_goal);
-
-//cout<< "pt_query:"<<endl;
-//cout << (pt_query.ptr())[0] << endl;
-//cout << (pt_query.ptr())[1] << endl;
-//cout<< "indices:"<<endl;
-//cout<< (indices.ptr())[0] << endl;
-//cout<< "dists:"<<endl;
-//cout<< (dists.ptr())[0] << endl;
-
-//flann::save_to_file(pt_query,"result.h5","pt_query");
-//flann::save_to_file(dataset,"result.h5","dataset");
-//flann::save_to_file(indices,"result.h5","indices");
-//flann::save_to_file(dists,"result.h5","dists");
