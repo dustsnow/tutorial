646d4184ecf23486db3888f572129dd4d7dcf10c
penghou <dustsnow@gmail.com>
2013-07-19 00:48:09 -0400

    Clean Up

--- rrt-2-link-arm/rrt-2-link-artm.cpp
+++ rrt-2-link-arm/rrt-2-link-artm.cpp
@@ -33,15 +33,15 @@ int l1 = 1.0;//length of link 1
 int l2 = 2.0;//length of link 2
 
 int w = 0.1;
-double step_size = 0.001;
+double step_size = 0.01;
 
 bool violation_flag = false;// Collision Checker result. if true, pick a random point as pt_target; else set pt_target to pt_goal
 bool obstacle_flag = true;// If true, do collision check; else, do direct approach
 
 GJKSolver_libccd solver;
 
-void calculateNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target,Matrix<double> pt_new);
-double calculateDistance(Matrix<double> pt1, Matrix<double> pt2);
+void calcNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target,Matrix<double> pt_new);
+double calcDistance(Matrix<double> pt1, Matrix<double> pt2);
 void workspaceConversion(Matrix<double> *workspace, Matrix<double> jointspace);
 void pi(Index<L2<double> > index,int a);
 void calcCenterPoint(Matrix<double> pt,Matrix<double> *cp);
@@ -50,6 +50,10 @@ void findLinkset(vector<Matrix<double> > linkset);
 void printLinkset(vector<Matrix<double> > linkset);
 
 int main(int argc, char** argv){
+	// Obstacles
+	//Box obs1(0.4,0.4,0);
+	//Transform3f tf_obs1(Vec3f(2.22,1,0));
+
 	// Center point for two links
 	Matrix<double> pt_cp(new double[4],2,2);
 	pt_cp[0][0] = 0;
@@ -77,16 +81,8 @@ int main(int argc, char** argv){
 	Index<L2<double> > index(dataset,flann::KDTreeIndexParams(4));
 	index.buildIndex();
 
-//    /// points that are searched for
-//	Matrix<double> pt_query(new double[2],1,2);
-
-
 	/// Target point matrix(randomly picked point or pt_goal point)
 	Matrix<double> pt_target(new double[2],1,2);
-
-
-	/// outpoint point 
-	Matrix<double> outpoint(new double[2],1,2);
 
 	/// zero point 
 	Matrix<double> pt_zero(new double[2],1,2);
@@ -134,7 +130,7 @@ int main(int argc, char** argv){
         pt_nearest[0][1] = (index.getPoint(indices[0][0]))[1];
 
 
-		calculateNewPoint(pt_nearest, pt_target, pt_new);
+		calcNewPoint(pt_nearest, pt_target, pt_new);
 
 		// If obstacles present, do collision check on new point
 		if(obstacle_flag){
@@ -145,7 +141,7 @@ int main(int argc, char** argv){
 		} 
 		/// Calculate distance between new point and pt_goal point(d). 
 		/// If d less than or equal to the threshold of the pt_goal point, this is the good enough result. Done
-		if(calculateDistance(pt_goal, pt_new) <= step_size) break;
+		if(calcDistance(pt_goal, pt_new) <= step_size) break;
 
 		/// If collision check pass, Add new point and new edge to tree
 		index.addPoints(pt_new);
@@ -164,10 +160,6 @@ int main(int argc, char** argv){
 			<< endl;
 	}
 	findLinkset(linkset);
-	printLinkset(linkset);
-	//Matrix<double> *data = linkset.data();
-	//getchar();
-
 	return 1;
 }
 /// Print the index
@@ -189,23 +181,24 @@ void ap(Matrix<double> *pt,double a, double b){
 	(*pt)[0][0] = a;
 	(*pt)[0][1] = b;
 }
-double calculateDistance(Matrix<double> pt1, Matrix<double> pt2){
+
+/// Calculate the distance of two points
+double calcDistance(Matrix<double> pt1, Matrix<double> pt2){
 	double x1 = pt1[0][0];
     double y1 = pt1[0][1];
     double x2 = pt2[0][0];
     double y2 = pt2[0][1];
-
 	double distance = sqrt(pow((x1 - x2),2)+pow((y1 - y2),2));
 	return distance;
 }
 
 /// Calculate the pt_new from pt_nearest and pt_target
-void calculateNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target,Matrix<double> pt_new){
+void calcNewPoint(Matrix<double> pt_nearest, Matrix<double> pt_target, Matrix<double> pt_new){
 	double x_nearest = pt_nearest[0][0];
 	double y_nearest = pt_nearest[0][1];
 	double x_target = pt_target[0][0];
 	double y_target = pt_target[0][1];
-	double distance = calculateDistance(pt_target,pt_nearest);
+	double distance = calcDistance(pt_target,pt_nearest);
 	pt_new[0][0] = (x_target - x_nearest)*step_size/distance+x_nearest;
 	pt_new[0][1] = (y_target - y_nearest)*step_size/distance+y_nearest;
 }
@@ -221,7 +214,9 @@ bool collisionCheck(Matrix<double> pt){
 	 */
 	// Obstacles
 	Box obs1(0.4,0.4,0);
-	Transform3f tf_obs1(Vec3f(1.22,2,0));
+	Transform3f tf_obs1(Vec3f(2.22,1.5,0));
+	//Box obs2(0.4,0.4,0);
+	//Transform3f tf_obs2(Vec3f(1.22,1.5,0));
 
 	// Calculate center point(cp)
 	Matrix<double> cp(new double[4],2,2);
@@ -256,9 +251,6 @@ void calcCenterPoint(Matrix<double> pt,Matrix<double> *cp){
 }
 void printLinkset(vector<Matrix<double> > linkset){
 	ofstream out("cpp_plot/linkset",ofstream::app);
-	//	out <<  pt_new[0][0] << " " << pt_new[0][1] << " " 
-	//		<<  pt_nearest[0][0] << " " << pt_nearest[0][1] << " " 
-	//		<< endl;
 
 	for(int i = 0; i < linkset.size(); i++){
 		out << linkset[i][0][0] << " "
